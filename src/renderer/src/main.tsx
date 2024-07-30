713530cae3fb34ed3bac5c1a8ddc131f816269a3
pompurin404 <pompurin404@mihomo.party>
2024-07-30 21:53:26 +0800

    disable annoying warnings

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { NextUIProvider } from '@nextui-org/react'
-
-import App from './App'
-import './assets/main.css'
+import '@renderer/utils/init'
+import '@renderer/assets/main.css'
+import App from '@renderer/App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
