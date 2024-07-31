056f07de00df6d4dd48942bf760690372f519065
pompurin404 <pompurin404@mihomo.party>
2024-07-31 12:38:32 +0800

    test autorun

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { NextUIProvider } from '@nextui-org/react'
 import '@renderer/utils/init'
@@ -11,9 +11,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </NextThemesProvider>
     </NextUIProvider>
   </React.StrictMode>
--- src/renderer/src/utils/api.ts
+++ src/renderer/src/utils/api.ts
@@ -1,3 +1,15 @@
 export async function mihomoVersion(): Promise<IMihomoVersion> {
   return await window.electron.ipcRenderer.invoke('mihomoVersion')
 }
+
+export async function checkAutoRun(): Promise<boolean> {
+  return await window.electron.ipcRenderer.invoke('checkAutoRun')
+}
+
+export async function enableAutoRun(): Promise<void> {
+  await window.electron.ipcRenderer.invoke('enableAutoRun')
+}
+
+export async function disableAutoRun(): Promise<void> {
+  await window.electron.ipcRenderer.invoke('disableAutoRun')
+}
