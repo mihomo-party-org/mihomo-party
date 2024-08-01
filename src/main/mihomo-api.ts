caa7c11537b45366d4ba1e3bc102bdd4cee14548
pompurin404 <pompurin404@mihomo.party>
2024-08-01 14:50:51 +0800

    connections and rules

--- src/main/mihomo-api.ts
+++ src/main/mihomo-api.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosInstance } from 'axios'
 import { controledMihomoConfig } from './config'
+import WebSocket from 'ws'
+import { window } from '.'
 
 let axiosIns: AxiosInstance = null!
+let mihomoTrafficWs: WebSocket = null!
 
-/// initialize some information
-/// enable force update axiosIns
 export const getAxios = async (force: boolean = false): Promise<AxiosInstance> => {
   if (axiosIns && !force) return axiosIns
 
@@ -32,8 +33,34 @@ export const mihomoConfig = async (): Promise<IMihomoConfig> => {
   return instance.get('/configs') as Promise<IMihomoConfig>
 }
 
-/// Update current configs
 export const patchMihomoConfig = async (patch: Partial<IMihomoConfig>): Promise<void> => {
   const instance = await getAxios()
   return instance.patch('/configs', patch)
+}
+
+export const mihomoConnections = async (): Promise<IMihomoConnectionsInfo> => {
+  const instance = await getAxios()
+  return instance.get('/connections') as Promise<IMihomoConnectionsInfo>
+}
+
+export const mihomoRules = async (): Promise<IMihomoRulesInfo> => {
+  const instance = await getAxios()
+  return instance.get('/rules') as Promise<IMihomoRulesInfo>
+}
+
+export const mihomoTraffic = (): void => {
+  let server = controledMihomoConfig['external-controller']
+  const secret = controledMihomoConfig.secret ?? ''
+  if (server?.startsWith(':')) server = `127.0.0.1${server}`
+
+  mihomoTrafficWs = new WebSocket(`ws://${server}/traffic?secret=${secret}`)
+
+  mihomoTrafficWs.onmessage = (e: { data: string }): void => {
+    window?.webContents.send('mihomoTraffic', JSON.parse(e.data) as IMihomoTrafficInfo)
+  }
+
+  mihomoTrafficWs.onerror = (): void => {
+    console.error('Traffic ws error')
+    mihomoConfig()
+  }
 }
