cd994e2b665dfd5e21df677c0ff7e79ba796c010
pompurin404 <pompurin404@outlook.com>
2024-08-03 21:44:07 +0800

    rule page

---
+++ src/renderer/src/components/rules/rule-item.tsx
@@ -0,0 +1,19 @@
+import { Card, CardBody } from '@nextui-org/react'
+import React from 'react'
+
+const RuleItem: React.FC<IMihomoRulesDetail> = (props) => {
+  const { type, payload, proxy } = props
+  return (
+    <Card className="m-2">
+      <CardBody className="flex justify-between">
+        <div className="flex justify-between">
+          <div className="select-none">{type}</div>
+          <div className="select-none">{payload}</div>
+          <div className="select-none">{proxy}</div>
+        </div>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default RuleItem
