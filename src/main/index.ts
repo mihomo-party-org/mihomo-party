fc6b86fabc54bc3cb1a0d4ce88523548a454017d
pompurin404 <pompurin404@mihomo.party>
2024-08-14 18:47:47 +0800

    try to fix macos start

--- src/main/index.ts
+++ src/main/index.ts
@@ -79,11 +79,13 @@ app.whenReady().then(async () => {
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
+    if (!mainWindow) {
       if (destroyTimer) {
         clearTimeout(destroyTimer)
       }
       createWindow(true)
+    } else {
+      showMainWindow()
     }
   })
 })
