062c4172084cdd156c284096a3e8addd74fe1b11
pompurin404 <pompurin404@mihomo.party>
2024-09-24 19:57:49 +0800

    fix portable updater

--- src/main/utils/init.ts
+++ src/main/utils/init.ts
@@ -105,7 +105,7 @@ async function cleanup(): Promise<void> {
   // update cache
   const files = await readdir(dataDir())
   for (const file of files) {
-    if (file.endsWith('.exe') || file.endsWith('.dmg')) {
+    if (file.endsWith('.exe') || file.endsWith('.dmg') || file.endsWith('.7z')) {
       try {
         await rm(path.join(dataDir(), file))
       } catch {
