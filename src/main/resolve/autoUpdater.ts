0ea63a78b416db4c09764f84152fae1728d2d4c8
pompurin404 <pompurin404@mihomo.party>
2024-09-26 13:21:21 +0800

    fix portable updater

--- src/main/resolve/autoUpdater.ts
+++ src/main/resolve/autoUpdater.ts
@@ -74,14 +74,17 @@ export async function downloadAndInstallUpdate(version: string): Promise<void> {
       }).unref()
     }
     if (file.endsWith('.7z')) {
-      await writeFile(
-        path.join(exeDir(), 'install.ps1'),
-        `& "${path.join(resourcesFilesDir(), '7za.exe')}" x -o"${exeDir()}" -y "${path.join(dataDir(), file)}"; Start-Process -FilePath "${exePath()}"`
-      )
-      spawn('powershell', ['-File', `"${path.join(exeDir(), 'install.ps1')}"`], {
-        shell: true,
-        detached: true
-      }).unref()
+      spawn(
+        'cmd',
+        [
+          '/C',
+          `""${path.join(resourcesFilesDir(), '7za.exe')}" x -o"${exeDir()}" -y "${path.join(dataDir(), file)}" & start "" "${exePath()}""`
+        ],
+        {
+          shell: true,
+          detached: true
+        }
+      ).unref()
       app.quit()
     }
     if (file.endsWith('.dmg')) {
