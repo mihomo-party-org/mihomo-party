704109daa4089019850f673c1726b48ead252bef
pompurin404 <pompurin404@mihomo.party>
2024-10-11 17:21:24 +0800

    update notify message

--- scripts/telegram.mjs
+++ scripts/telegram.mjs
@@ -5,6 +5,7 @@ const chat_id = '@mihomo_party_group'
 const pkg = readFileSync('package.json', 'utf-8')
 const changelog = readFileSync('changelog.md', 'utf-8')
 const { version } = JSON.parse(pkg)
+const downloadUrl = `https://github.com/mihomo-party-org/mihomo-party/releases/download/v${version}`
 let content = `<b>🌟 <a href="https://github.com/mihomo-party-org/mihomo-party/releases/tag/v${version}">Mihomo Party v${version}</a> 正式发布</b>\n\n`
 for (const line of changelog.split('\n')) {
   if (line.length === 0) {
@@ -15,6 +16,20 @@ for (const line of changelog.split('\n')) {
     content += `${line}\n`
   }
 }
+
+content += '\n<b>下载地址：</b>\n<b>Windows10/11：</b>\n'
+content += `安装版：<a href="${downloadUrl}/mihomo-party-windows-${version}-x64-setup.exe">64位</a> | <a href="${downloadUrl}/mihomo-party-windows-${version}-ia32-setup.exe">32位</a> | <a href="${downloadUrl}/mihomo-party-windows-${version}-arm64-setup.exe">ARM64</a>\n`
+content += `便携版：<a href="${downloadUrl}/mihomo-party-windows-${version}-x64-portable.7z">64位</a> | <a href="${downloadUrl}/mihomo-party-windows-${version}-ia32-portable.7z">32位</a> | <a href="${downloadUrl}/mihomo-party-windows-${version}-arm64-portable.7z">ARM64</a>\n`
+content += '\n<b>Windows7/8：</b>\n'
+content += `安装版：<a href="${downloadUrl}/mihomo-party-win7-${version}-x64-setup.exe">64位</a> | <a href="${downloadUrl}/mihomo-party-win7-${version}-ia32-setup.exe">32位</a>\n`
+content += `便携版：<a href="${downloadUrl}/mihomo-party-win7-${version}-x64-portable.7z">64位</a> | <a href="${downloadUrl}/mihomo-party-win7-${version}-ia32-portable.7z">32位</a>\n`
+content += '\n<b>macOS：</b>\n'
+content += `DMG：<a href="${downloadUrl}/mihomo-party-macos-${version}-x64.dmg
+">Intel</a> | <a href="${downloadUrl}/mihomo-party-macos-${version}-arm64.dmg">Apple Silicon</a>\n`
+content += '\n<b>Linux：</b>\n'
+content += `DEB：<a href="${downloadUrl}/mihomo-party-linux-${version}-amd64.deb
+">64位</a> | <a href="${downloadUrl}/mihomo-party-linux-${version}-arm64.deb">ARM64</a>\n`
+content += `RPM：<a href="${downloadUrl}/mihomo-party-linux-${version}-x86_64.rpm">64位</a> | <a href="${downloadUrl}/mihomo-party-linux-${version}-aarch64.rpm">ARM64</a>`
 
 const { data: messageData } = await axios.post(
   `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -2,11 +2,25 @@ import yaml from 'yaml'
 import { readFileSync, writeFileSync } from 'fs'
 
 const pkg = readFileSync('package.json', 'utf-8')
-const changelog = readFileSync('changelog.md', 'utf-8')
+let changelog = readFileSync('changelog.md', 'utf-8')
 const { version } = JSON.parse(pkg)
+const downloadUrl = `https://github.com/mihomo-party-org/mihomo-party/releases/download/v${version}`
 const latest = {
   version,
   changelog
 }
 
+changelog += '\n### 下载地址：\n\n#### Windows10/11：\n\n'
+changelog += `- 安装版：[64位](${downloadUrl}/mihomo-party-windows-${version}-x64-setup.exe) | [32位](${downloadUrl}/mihomo-party-windows-${version}-ia32-setup.exe) | [ARM64](${downloadUrl}/mihomo-party-windows-${version}-arm64-setup.exe)\n\n`
+changelog += `- 便携版：[64位](${downloadUrl}/mihomo-party-windows-${version}-x64-portable.7z) | [32位](${downloadUrl}/mihomo-party-windows-${version}-ia32-portable.7z) | [ARM64](${downloadUrl}/mihomo-party-windows-${version}-arm64-portable.7z)\n\n`
+changelog += '\n#### Windows7/8：\n\n'
+changelog += `- 安装版：[64位](${downloadUrl}/mihomo-party-win7-${version}-x64-setup.exe) | [32位](${downloadUrl}/mihomo-party-win7-${version}-ia32-setup.exe)\n\n`
+changelog += `- 便携版：[64位](${downloadUrl}/mihomo-party-win7-${version}-x64-portable.7z) | [32位](${downloadUrl}/mihomo-party-win7-${version}-ia32-portable.7z)\n\n`
+changelog += '\n#### macOS：\n\n'
+changelog += `- DMG：[Intel](${downloadUrl}/mihomo-party-macos-${version}-x64.dmg) | [Apple Silicon](${downloadUrl}/mihomo-party-macos-${version}-arm64.dmg)\n\n`
+changelog += '\n#### Linux：\n\n'
+changelog += `- DEB：[64位](${downloadUrl}/mihomo-party-linux-${version}-amd64.deb) | [ARM64](${downloadUrl}/mihomo-party-linux-${version}-arm64.deb)\n\n`
+changelog += `- RPM：[64位](${downloadUrl}/mihomo-party-linux-${version}-x86_64.rpm) | [ARM64](${downloadUrl}/mihomo-party-linux-${version}-aarch64.rpm)`
+
 writeFileSync('latest.yml', yaml.stringify(latest))
+writeFileSync('changelog.md', changelog)
