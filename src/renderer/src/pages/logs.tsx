cbf5febc9409192f12df0e970e7cd3b419f7eeda
qianlongzt <18493471+qianlongzt@users.noreply.github.com>
2024-10-31 22:52:38 +0800

    feat: log level filter

--- src/renderer/src/pages/logs.tsx
+++ src/renderer/src/pages/logs.tsx
@@ -1,7 +1,7 @@
 import BasePage from '@renderer/components/base/base-page'
 import LogItem from '@renderer/components/logs/log-item'
 import { useEffect, useMemo, useRef, useState } from 'react'
-import { Button, Divider, Input } from '@nextui-org/react'
+import { Button, Divider, Input, Select, SelectItem } from '@nextui-org/react'
 import { Virtuoso, VirtuosoHandle } from 'react-virtuoso'
 import { IoLocationSharp } from 'react-icons/io5'
 import { CgTrash } from 'react-icons/cg'
@@ -33,19 +33,34 @@ window.electron.ipcRenderer.on('mihomoLogs', (_e, log: IMihomoLogInfo) => {
     cachedLogs.trigger(cachedLogs.log)
   }
 })
+let memoLogLevel = new Set<LogLevel>()
 
 const Logs: React.FC = () => {
   const [logs, setLogs] = useState<IMihomoLogInfo[]>(cachedLogs.log)
   const [filter, setFilter] = useState('')
   const [trace, setTrace] = useState(true)
+  const [logLevel, setLogLevel] = useState(memoLogLevel)
+
+  const handleSetLogLevel = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+    const t = e.target.value
+    const nextLogLevel =
+      t === '' ? new Set<LogLevel>(null) : new Set(t.split(',').map((e) => e as LogLevel))
+
+    setLogLevel(nextLogLevel)
+    memoLogLevel = nextLogLevel
+  }
 
   const virtuosoRef = useRef<VirtuosoHandle>(null)
   const filteredLogs = useMemo(() => {
-    if (filter === '') return logs
+    if (logLevel.size === 0 && filter === '') {
+      return logs
+    }
     return logs.filter((log) => {
-      return includesIgnoreCase(log.payload, filter) || includesIgnoreCase(log.type, filter)
+      const lv = logLevel.size > 0 ? logLevel.has(log.type) : true
+      const match = includesIgnoreCase(log.payload, filter) || includesIgnoreCase(log.time, filter)
+      return lv && match
     })
-  }, [logs, filter])
+  }, [logs, filter, logLevel])
 
   useEffect(() => {
     if (!trace) return
@@ -71,6 +86,20 @@ const Logs: React.FC = () => {
     <BasePage title="实时日志">
       <div className="sticky top-0 z-40">
         <div className="w-full flex p-2">
+          <Select
+            classNames={{ trigger: 'data-[hover=true]:bg-default-200' }}
+            className="w-[150px]"
+            placeholder="日志等级"
+            size="sm"
+            selectedKeys={logLevel}
+            selectionMode="multiple"
+            onChange={handleSetLogLevel}
+          >
+            <SelectItem key="error">错误</SelectItem>
+            <SelectItem key="warning">警告</SelectItem>
+            <SelectItem key="info">信息</SelectItem>
+            <SelectItem key="debug">调试</SelectItem>
+          </Select>
           <Input
             size="sm"
             value={filter}
