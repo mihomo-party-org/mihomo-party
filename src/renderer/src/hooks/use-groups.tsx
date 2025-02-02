35c239e02d11419bf98d2aeb2f53cad3f54eae0e
pompurin404 <pompurin404@mihomo.party>
2025-02-03 07:42:04 +0800

    fix: auto-update latency test results in UI without requiring any click

--- src/renderer/src/hooks/use-groups.tsx
+++ src/renderer/src/hooks/use-groups.tsx
@@ -12,7 +12,8 @@ const GroupsContext = createContext<GroupsContextType | undefined>(undefined)
 export const GroupsProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { data: groups, mutate } = useSWR<IMihomoMixedGroup[]>('mihomoGroups', mihomoGroups, {
     errorRetryInterval: 200,
-    errorRetryCount: 10
+    errorRetryCount: 10,
+    refreshInterval: 1000
   })
 
   React.useEffect(() => {
